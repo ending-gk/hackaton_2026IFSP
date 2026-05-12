2786d1be293bd6f3991ada37f75d910329c4f359
itoikk <itoiandre14@gmail.com>
2026-05-12 17:12:37 -0300

    funcao criar usuario

---
+++ scripts/scripts1.js
@@ -0,0 +1,8 @@
+function criar_usuario() {
+    const usuario = {
+    email = document.getElementById("email").value,
+    senha = document.getElementById("senha").value,
+    nome_usuario = document.getElementById("nome_usuario").value,
+    prontuario = document.getElementById("prontuario").value
+    };
+}
